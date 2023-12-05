5e186431bd056ed522d6fc638738a8083c8b6b03
lekshmisrt <151364992+lekshmisrt@users.noreply.github.com>
2023-12-05 19:08:49 +0530

    Update display.java

--- DecJenkin/src/main/java/pack/display.java
+++ DecJenkin/src/main/java/pack/display.java
@@ -5,6 +5,8 @@ public class display {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("display");
+		System.out.println("Hello December");
+		
 
 	}
 
